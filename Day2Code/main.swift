5a549d0d44686988d2485e6165f07703550a9f9d
sushmitha <sushmitha51094@gmail.com>
2019-02-08 18:49:47 -0500

    string handling functions

--- Day2Code/main.swift
+++ Day2Code/main.swift
@@ -10,4 +10,34 @@ import Foundation
 
 print("Hello, World!")
 
+//*if(true<false)
+//{
+  //  print("Thanks")
+
+//}
+
+var s:String = "hello"
+var str = String()
+str = "hello world"
+var p:String
+
+p = "call me"
+
+print(s,str,p)
+print(s.count)
+print(str.description)
+print(str.hasPrefix("hello"))
+print(p.hasSuffix("me"))
+print(s.hasPrefix("hello"))
+print(str.uppercased())
+print(str.customMirror)
+
+
+
+
+
+
+
+
+
 

633556967651d7a5496734a3d4f46850b7575124
MacStudent <macstudent@126-iMac12.local>
2019-02-08 18:11:35 -0500

    Initial Commit

---
+++ Day2Code/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day2Code
+//
+//  Created by MacStudent on 2019-02-08.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
